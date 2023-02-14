cf59f2096752e6090c77e694ac5b34005e3adf46
Joseph <Jcarbon3@stevens.edu>
2023-02-13 21:39:32 -0500

    Added browse page

---
+++ lib/browse_page.dart
@@ -0,0 +1,81 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, unused_element, no_logic_in_create_state, prefer_const_constructors
+
+import "package:flutter/material.dart";
+import "package:two_deux/login_page.dart";
+
+class BrowsePage extends StatefulWidget {
+  const BrowsePage({Key? key}) : super(key: key);
+
+  @override
+  State<BrowsePage> createState() => _BrowsePageState();
+}
+
+class _BrowsePageState extends State<BrowsePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[400],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              //browse
+              SizedBox(height: 50),
+
+              Text(
+                "Browse Stories",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26,
+                ),
+              ),
+              Padding(padding: const EdgeInsets.symmetric(vertical: 15)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context){
+                                return LoginPage();
+                              },
+                            ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10), color: Colors.white, border: Border.all(color: Colors.black, width: 5)
+                      ),
+                      child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                              "Story 1",
+                              style: TextStyle(color: Colors.black, fontSize: 25),
+                          ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),color: Colors.white, border: Border.all(color: Colors.black, width: 5)
+                    ),
+                    child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "Story 2",
+                          style: TextStyle(color: Colors.black, fontSize: 25),
+                        ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
